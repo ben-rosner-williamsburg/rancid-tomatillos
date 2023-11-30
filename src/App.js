823d5f10cb1f5ded042dc5da3d46982fa89bdb82
Zen McMillan <zenmcmillan@gmail.com>
2023-11-30 17:32:54 -0600

    Display movie on DOM

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-
 import './App.css';
+import movieData from "./data";
+import Movies from './movies/movies';
+import { useState, useEffect} from "react"
+
 
 function App() {
-  
+  const [movies, setMovies] = useState([]);
+
+  return (
+    <main>
+      <h1>Rancid Tomatillos</h1>
+      <div>
+        <Movies movieData={movieData}/> 
+      </div>
+    </main>
+  )
 }
 
 export default App;
